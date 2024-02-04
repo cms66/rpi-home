3110718687bfaceafc249f4c99842de8463d1155
cms66 <58144229+cms66@users.noreply.github.com>
2024-02-04 09:04:12 +0000

    Update rpi_setup_base.sh

--- rpi_setup_base.sh
+++ rpi_setup_base.sh
@@ -26,6 +26,10 @@ python -m venv --system-site-packages /home/$usrname/.venv
 echo "alias myvp=\"source ~/.venv/bin/activate\"" >> /home/$usrname/.bashrc
 chown -R $usrname:$usrname /home/$usrname/.venv
 
+# create local folder structure for created user with code examples
+tar -xvzf /home/$usrname/.pisetup/rpi-home/local.tgz -C /home/$usrname
+chown -R $usrname:$usrname /home/$usrname/local/
+
 # Configure fail2ban
 cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
 cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
@@ -34,6 +38,20 @@ cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 ufw allow 22
 ufw logging on
 ufw enable
+
+# Networking
+piname=$(hostname)
+echo "127.0.0.1   $piname.local $piname" >> /etc/hosts
+localip=$(hostname -I | awk '{print $1}')
+echo "$localip   $piname.local $piname" >> /etc/hosts
+sed -i "s/rootwait/rootwait ipv6.disable=1/g" /boot/firmware/cmdline.txt
+
+# Disable root SSH login
+sed -i 's/#PermitRootLogin\ prohibit-password/PermitRootLogin\ no/g' /etc/ssh/sshd_config
+
+# Set default shell to bash
+echo "dash dash/sh boolean false" | debconf-set-selections
+DEBIAN_FRONTEND=noninteractive dpkg-reconfigure dash
 
 # Reboot or Poweroff (if static IP setup needed on router)
 read -p "Finished base setup, press p to poweroff (if setting a static IP on router) or any other key to reboot, then login as $usrname: " input
