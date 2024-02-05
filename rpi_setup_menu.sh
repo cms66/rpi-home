b5af4cfccffb13333ffacee19449e9a44dd0a6c2
cms66 <58144229+cms66@users.noreply.github.com>
2024-02-05 16:43:33 +0000

    Update rpi_setup_menu.sh
    Removed OpenCL option

--- rpi_setup_menu.sh
+++ rpi_setup_menu.sh
@@ -7,7 +7,7 @@ show_main_menu()
 {
 	clear
 	printf "Setup Main menu \n--------------\n"
-	printf "select setup option or x to exit \n 1) Hardware \n 2) NFS \n 3) SSH - Shared keys \n 4) OpenMPI \n 5) OpenCV \n 6) OpenCL \n 7) Update setup \n 8) Update system \n 9) System summary \n"
+	printf "select setup option or x to exit \n 1) Hardware \n 2) NFS \n 3) SSH - Shared keys \n 4) OpenMPI \n 5) OpenCV \n 6) Update setup \n 7) Update system \n 8) System summary \n"
 }
 
 usrname=$(logname)
@@ -25,11 +25,10 @@ while [ $n != "x" ]; do
 		2) sh /home/$usrname/.pisetup/rpi-home/rpi_setup_nfs.sh;;
 		3) sh /home/$usrname/.pisetup/rpi-home/rpi_setup_ssh_keys.sh;;
 		4) sh /home/$usrname/.pisetup/rpi-home/rpi_setup_openmpi.sh;;
-		5) sh /home/$usrname/.pisetup/rpi-home/rpi_setup_opencv.sh;;
-		6) sh /home/$usrname/.pisetup/rpi-home/rpi_setup_opencl.sh;;
-  		7) sh /home/$usrname/.pisetup/rpi-home/rpi_setup_git_pull_setup.sh;;
-    		8) sh /home/$usrname/.pisetup/rpi-home/rpi_setup_update_system.sh;;
-      		9) sh /home/$usrname/.pisetup/rpi-home/rpi_setup_system_summary.sh;;
+		5) sh /home/$usrname/.pisetup/rpi-home/rpi_setup_opencv.sh;;;
+  		6) sh /home/$usrname/.pisetup/rpi-home/rpi_setup_git_pull_setup.sh;;
+    		7) sh /home/$usrname/.pisetup/rpi-home/rpi_setup_update_system.sh;;
+      		8) sh /home/$usrname/.pisetup/rpi-home/rpi_setup_system_summary.sh;;
 		*) read -p "invalid option - press enter to return to menu" errkey;;
 	esac
 	show_main_menu
