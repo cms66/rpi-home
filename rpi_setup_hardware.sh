c58a3da5baf1f954ae75b2991ec7f239ffba16e9
cms66 <58144229+cms66@users.noreply.github.com>
2024-02-06 21:53:34 +0000

    Update rpi_setup_hardware.sh

--- rpi_setup_hardware.sh
+++ rpi_setup_hardware.sh
@@ -1,1 +1,10 @@
+# Hardware setup
+# TODO
+# - Check for attached devices and configs for previous devices
+# - Enable port on firewall for multiple camera streams
+# - Check RPi model for GPS setup
+# - Create setup options for GPS using UART/I2C/SPI
+# - Use GPIO pins for fan/cooling
 
+# 1 CSI Camera
+# 2 USB Camera
