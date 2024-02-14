eb809d020607e0065be902cda677d9d718e6bd22
cms66 <58144229+cms66@users.noreply.github.com>
2024-02-14 19:39:40 +0000

    Update rpi_setup_hardware.sh

--- rpi_setup_hardware.sh
+++ rpi_setup_hardware.sh
@@ -20,3 +20,29 @@ show_hardware_menu()
    	4) Calibrate Sense Hat \n\
     	5) Arduino - USB \n 6) Arduino I2C \n 7) GPS \n 8) TFT LCD \n 9) Calibrate display \n 10) Bluetooth \n 11) Robotic arm \n 12) DVB TV Hat \n 13) Audio (for Desktop image) \n 14) Arduino Libraries \n 15) Pressure sensor \n 16) Setup I2C \n 16) Setup GPS - PA1010D \n"
 }
+show_hardware_menu
+read -p "Select option or x to exit to main menu: " n
+while [ $n != "x" ]; do
+	case $n in
+		1) setup_cam_csi;;
+		2) setup_cam_usb;;
+		3) setup_sense_hat;;
+		4) calib_sense_hat;;
+		5) setup_arduino_usb;;
+		6) setup_arduino_i2c;;
+		7) setup_gps;;
+		8) setup_tft_lcd;;
+		9) calib_display;;
+		10) setup_bluetooth;;
+		11) setup_robot_arm;;
+		12) setup_dvb_tv;;
+		13) setup_audio_desktop;;
+		14) setup_arduino_libs;;
+		15) setup_pressure_sensor;;
+  		16) setup_i2c;;
+    		17) setup_PA1010D;;
+		*) read -p "invalid option - press enter to continue" errkey;;
+	esac
+	show_hardware_menu
+	read -p "Select option or x to exit to main menu: " n
+done
