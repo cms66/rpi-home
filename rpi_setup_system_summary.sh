fbdb7b4f50626786eb4390fd8e6646fe31eadef4
cms66 <58144229+cms66@users.noreply.github.com>
2024-02-16 14:29:59 +0000

    Update rpi_setup_system_summary.sh

--- rpi_setup_system_summary.sh
+++ rpi_setup_system_summary.sh
@@ -3,7 +3,7 @@ printf "System summary \n--------------\n"
 printf "Model: $pimodel \n"
 printf "Revision: $pirev \n"
 printf "Architecture: $osarch \n"
-printf "Memory: $pimem \n"
+printf "Memory: \n$pimem \n"
 printf "Firewall "
 ufw status
 read -p "Press enter to return to menu" input
